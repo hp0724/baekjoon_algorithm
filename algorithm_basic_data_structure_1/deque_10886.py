d7ca40551c06849238583b6a6ceb146b52cb6511
suha <hp980724@gmail.com>
2023-05-27 14:42:01 +0900

    알고리즘 기초 자료구조 1 완료

---
+++ algorithm_basic_data_structure_1/deque_10886.py
@@ -0,0 +1,41 @@
+from collections import deque
+import sys
+
+q = deque()
+output = []
+for _ in range(int(sys.stdin.readline())):
+    cmd = sys.stdin.readline().split()
+
+    if cmd[0] == "push_back":
+        q.append(cmd[1])
+    elif cmd[0] == "push_front":
+        q.appendleft(cmd[1])
+    elif cmd[0] == "pop_front":
+        if q:
+            output.append(q.popleft())
+        else:
+            output.append("-1")
+    elif cmd[0] == "pop_back":
+        if q:
+            output.append(q.pop())
+        else:
+            output.append("-1")
+    elif cmd[0] == "back":
+        if q:
+            output.append(q[-1])
+        else:
+            output.append("-1")
+    elif cmd[0] == "front":
+        if q:
+            output.append(q[0])
+        else:
+            output.append("-1")
+    elif cmd[0] == "size":
+        output.append(len(q))
+    elif cmd[0] == "empty":
+        if q:
+            output.append("0")
+        else:
+            output.append("1")
+
+print("\n".join(map(str, output)))
