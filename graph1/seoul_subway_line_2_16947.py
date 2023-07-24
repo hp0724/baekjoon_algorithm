2dc7e641240bf5d7b51b48a4d100a6975b43dccb
suha <hp980724@gmail.com>
2023-07-24 12:09:34 +0900

    07_24_2

--- graph1/seoul_subway_line_2_16947.py
+++ graph1/seoul_subway_line_2_16947.py
@@ -1,61 +1,66 @@
-import sys 
-from collections import deque 
+from collections import deque
+import sys
+sys.setrecursionlimit(10**9)
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
 
-# 순환선이 존재 확인 
-# dfs 
-def circulation_station(start,idx,cnt):
-    global cycle 
-    # 순환선 체크 
-    if start == idx and cnt >=2:
-        cycle =True 
-        return 
-    #현재 역 방문 표시 
-    visited[idx] =True
-    # 다음 방문할 역을 받고 
-    for i in info[idx] :
-        if not visited[i]:
-            circulation_station(start,i,cnt+1)
-        elif i == start and cnt>=2:
-            circulation_station(start,i,cnt)
-# 역과 순환역 사이 거리체크는 bfs 
-def distance_station():
-    global check 
-    q= deque()
-    # 순환역에 속하면 0 
-    for i in range(N):
-        if cycle_station[i]:
-            check[i] =0 
-            q.append(i)
-    while q : 
-        now= q.popleft()
-        for i in info[now] :
-            if check[i]==-1:
-                q.append(i)
-                # 이동 거리 구하기 
-                check[i] = check[now] +1 
-    
+n = int(input())
 
-    for i in check: 
-        print(i,end=' ')
+graph = [[] for _ in range(n+1)]
+
+cycle = set()
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+visited = [False] * (n + 1)
+
+cy = False
+
+def dfs(start, here, level, li):
+    global cy
+    if level <= 2:
+        for v in graph[here]:
+            if visited[v] == False:
+                visited[v] = True
+                dfs(start, v, level+1, li+[v])
+                visited[v] = False
+    else:
+        for v in graph[here]:
+            if visited[v] == False:
+                visited[v] = True
+                dfs(start, v, level+1, li+[v])
+                visited[v] = False
+            else:
+                if v == start:
+                    cy = True
+                    for l in li:
+                        cycle.add(l)
+                    return
+
+for i in range(1, n+1):
+    if cy == True:
+        break
+    visited[i] = True
+    dfs(i, i, 1, [i])
+    visited[i] = False
+
+cycle = list(cycle)
+# bfs 수행
+answer = [int(1e9)] * (n + 1)
+q = deque()
+for c in cycle:
+    q.append((c, 0))
+    answer[c] = 0
+
+while q:
+    node, level = q.popleft()
+
+    for v in graph[node]:
+        if answer[v] == int(1e9):
+            answer[v] = level+1
+            q.append((v, level+1))
 
 
-N=int(input())
-info=[[] for _ in range(N)]
-cycle_station = [False] * N 
-check =[-1]*N
-for _ in range(N):
-    a,b= map(int,input().split())
-    info[a-1].append(b-1)
-    info[b-1].append(a-1)
-
-for i in range(N):
-    visited = [False] *N 
-    cycle = False
-    circulation_station(i,i,0)
-    if cycle :
-        cycle_station[i]=True 
-
-
-distance_station()
+print(" ".join(map(str, answer[1:])))
